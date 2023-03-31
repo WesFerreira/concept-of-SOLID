ce75bb424986b958d8a631860ee9da6fb5f88dc1
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-03-30 23:53:26 -0300

    created the class ReajusteServico and put the method reajustarSalario

---
+++ src/br/com/alura/rh/service/ReajusteServico.java
@@ -0,0 +1,17 @@
+package br.com.alura.rh.service;
+
+
+
+public class ReajusteService {
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumeto) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        funcionario.atualizarSalario(salarioReajustado);
+    }
+
+}
